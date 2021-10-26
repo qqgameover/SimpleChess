a513c0b0e61ae1d0a221e9ac4b90d3bf0eb3419a
qqgameover <kasper35@live.no>
2021-10-26 12:43:45 +0200

    ok.

--- SimpleChess/Piece.cs
+++ SimpleChess/Piece.cs
@@ -2,27 +2,15 @@
 
 namespace SimpleChess
 {
-    class Piece
+    abstract class Piece
     {
         public string Symbol { get; }
-        private readonly string _type;
 
-        public Piece(string type, string symbol)
+        protected Piece(string symbol)
         {
-            _type = type;
             Symbol = symbol;
         }
 
-        public bool Move(string fromPosition, string toPosition)
-        {
-            if (_type == "Rook") return fromPosition[0] == toPosition[0] || fromPosition[1] == toPosition[1];
-            if (_type == "Bishop")
-            {
-                var diffCol = fromPosition[0] - toPosition[0];
-                var diffRow = fromPosition[1] - toPosition[1];
-                return Math.Abs(diffRow) == Math.Abs(diffCol);
-            }
-            return true;
-        }
+        public abstract bool Move(string fromPosition, string toPosition);
     }
 }
--- SimpleChess/Program.cs
+++ SimpleChess/Program.cs
@@ -10,8 +10,10 @@ namespace SimpleChess
         {
             Console.OutputEncoding = Encoding.UTF8;
             var board = new Board();
-            var bishop = new Piece("Bishop", "LPR");
-            var rook = new Piece("Rook", "TRN");
+            //var bishop = new Piece("Bishop", "LPR");
+            //var rook = new Piece("Rook", "TRN");
+            var bishop = new Bishop();
+            var rook = new Rook();
             board.Set("e4", bishop);
             board.Set("f7", rook);
             while (true)
