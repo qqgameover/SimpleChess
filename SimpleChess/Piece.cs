8322a5c5d7bfdb6f8a3372f8381eb21c9cafdaf8
qqgameover <kasper35@live.no>
2021-10-26 12:27:09 +0200

    Initial commit

---
+++ SimpleChess/Piece.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SimpleChess
+{
+    class Piece
+    {
+        public string Symbol { get; }
+        private readonly string _type;
+
+        public Piece(string type, string symbol)
+        {
+            _type = type;
+            Symbol = symbol;
+        }
+
+        public bool Move(string fromPosition, string toPosition)
+        {
+            if (_type == "Rook") return fromPosition[0] == toPosition[0] || fromPosition[1] == toPosition[1];
+            if (_type == "Bishop")
+            {
+                var diffCol = fromPosition[0] - toPosition[0];
+                var diffRow = fromPosition[1] - toPosition[1];
+                return Math.Abs(diffRow) == Math.Abs(diffCol);
+            }
+            return true;
+        }
+    }
+}
